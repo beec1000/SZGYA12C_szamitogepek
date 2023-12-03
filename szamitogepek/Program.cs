2c3ed38b88bdd195ba2d4916a551cba43428db77
beec1000 <beec1000.minecraft@gmail.com>
2023-12-03 22:28:01 +0100

    Add project files.

---
+++ szamitogepek/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+namespace szamitogepek
+{
+    public class Program
+    {
+        static List<Szamitogep> F7(List<Szamitogep> sz)
+        {
+            return sz.Where(d => d.OS.Contains("Windows") && d.KijelzoMeret > 20).ToList();
+        }
+
+        static List<Szamitogep> F8(List<Szamitogep> sz)
+        {
+            var x = sz.Min(d => d.Sebesseg);
+            var legmagasabb = sz.Where(d => d.Sebesseg == x).ToList();
+            return legmagasabb;
+        }
+
+        static double F9(List<Szamitogep> sz)
+        {
+            return sz.Average(d => d.RAM);
+        }
+
+        static List<string> F10(List<Szamitogep> sz)
+        {
+            var x = sz.Where(d => d.Tipus == "DDR4");
+            return x.Select(d => d.Gyarto).Distinct().OrderBy(d => d).ToList();
+        }
+
+        static List<Szamitogep> F11(List<Szamitogep> sz)
+        {
+            return sz.Where(d => d.CPU.Contains("Intel") && d.KijelzoMeret < 20).ToList();
+        }
+
+        static void Main(string[] args)
+        {
+            var szamitogepek = new List<Szamitogep>();
+
+            using var sr = new StreamReader(@"..\..\..\src\szamitogepek.txt");
+            {
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    szamitogepek.Add(new Szamitogep(sr.ReadLine()));
+                }
+            }
+
+            foreach (var i in szamitogepek) Console.WriteLine(i);
+
+            Console.WriteLine("7.feladat");
+            var f7 = F7(szamitogepek);
+            if (f7.Count > 0)
+            {
+                Console.WriteLine($"{f7.Count}db ilyen gép van");
+            }
+            else
+            {
+                Console.WriteLine("HIBA 404!");
+            }
+
+            Console.WriteLine("8.feladat");
+            var f8 = F8(szamitogepek);
+            foreach (var i in f8)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"{f8.First().Sebesseg} Ghz a leglassabb sebesség, {f8.Count} db");
+
+            Console.WriteLine("9.feladat");
+            Console.WriteLine($"{Math.Round(F9(szamitogepek)), 2} GB az átlag memóriaméret.");
+
+            Console.WriteLine("10.feladat");
+            var f10 = F10(szamitogepek);
+            if (f10.Count == 0)
+            {
+                Console.WriteLine("HIBA 404!");
+            }
+            else
+            {
+                Console.WriteLine($"gyártók: {string.Join(", ", f10)}");
+            }
+
+            Console.WriteLine("11.feladat");
+            Console.WriteLine($"{string.Join(", ", F11(szamitogepek).Select(d => d.Azonosito))}");
+            
+            using var sw = new StreamWriter(@"..\..\..\src\szamitogepekCm.txt");
+            {
+                foreach (var i in szamitogepek)
+                {
+                    sw.WriteLine($"{i.Gyarto}{i.Modell}, {Math.Round(i.KijelzoMeretCm, 2)}cm");
+                }
+            }
+        }
+    }
+}
---
+++ szamitogepek/Szamitogep.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamitogepek
+{
+    public class Szamitogep
+    {
+        public int Azonosito { get; set; }
+        public string CPU { get; set; }
+        public double Sebesseg { get; set; }
+        public int RAM { get; set; }
+        public string Tipus { get; set; }
+        public string OS { get; set; }
+        public string Gyarto { get; set; }
+        public string Modell { get; set; }
+        public double KijelzoMeret { get; set; }
+
+        public double KijelzoMeretCm => this.KijelzoMeret * 2.54;
+
+        public Szamitogep(string s)
+        {
+            string[] v = s.Split('|');
+            this.Azonosito = int.Parse(v[0]);
+            this.CPU = v[1];
+            this.Sebesseg = double.Parse(v[2]);
+            string[] ramAdatok = v[3].Split(' ');
+            this.RAM = int.Parse(ramAdatok.First());
+            this.Tipus = ramAdatok.Last();
+            this.OS = v[4];
+            this.Gyarto = v[5].Split(' ').First();
+            this.Modell = v[5].Split(' ').Last();
+            this.KijelzoMeret = double.Parse(v[6]);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Azonosito}, {this.CPU}, {this.Sebesseg}, {this.RAM}GB {this.Tipus}, {this.OS}, {this.Gyarto}, {this.Modell}, {this.KijelzoMeret}";
+        }
+    }
+}
